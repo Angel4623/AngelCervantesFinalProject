e9fcdc04839c53d9d6119320964295ac1bbf80da
Angel4623 <112006834+Angel4623@users.noreply.github.com>
2024-05-09 17:19:00 -0600

    Delete States directory

--- States/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const authController = require("../controllers/authController");
-
-router.post("/", registerController.handelNewUser);
-
-module.exports = router;
--- States/routes/register.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express();
-const registerController = require("../controllers/registerController");
-
-router.post("/", registerController.handelNewUser);
-
-module.exports = router;
